719e977f5b6e302f5e2d30e387b6c9269136aa26
Oran Simhony <oran.simhony@gmail.com>
2019-05-23 21:14:15 +0300

    Added functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import marked from 'marked';
 
 const App: React.FC = () => {
+  const [text, setText] = useState('');
+
+  function parseMarkdown(text: string) {
+    return { '__html': marked(text, { sanitize: true }) };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 style={{ color: '#00adb5' }}>Markdown Previewer</h1>
+      <div id="body">
+        <textarea placeholder='Type Something' style={{ width: '50%', height: window.innerHeight - 100 }} value={text} onChange={(e) => { setText(e.target.value) }} />
+        <div id='viewer'>
+          <div id='viewerBody' style={{ width: '50%', height: window.innerHeight - 100 }} dangerouslySetInnerHTML={parseMarkdown(text)} />
+        </div>
+      </div>
     </div>
   );
 }
